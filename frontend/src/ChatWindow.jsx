187fed01c65a279edbdc417012f43b7270bc6f82
rachittpatel <rachitpatel6337@gmail.com>
2025-08-21 09:40:11 +0530

    frontend deploy

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -26,7 +26,8 @@ function ChatWindow() {
         setNewChat(false);
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", {
+           const response = await fetch(`${import.meta.env.VITE_API_URL}/api/chat`, {
+
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
